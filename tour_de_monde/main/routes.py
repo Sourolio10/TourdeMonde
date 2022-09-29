decd99c9a87fa59cae5951f87d740c5eb2489f51
JoshSensanb <111921798+JoshSensanb@users.noreply.github.com>
2022-09-29 16:51:44 -0400

    Removing accidental commit

--- tour_de_monde/main/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-from iot_security import limiter
-main = Blueprint('main', __name__)
-
-
-@main.route('/')
-@limiter.exempt
-def index():
-    return render_template('main/index.html')
--- tour_de_monde/models/utils.py
+++
@@ -1,9 +0,0 @@
-import string
-import random
-
-def rand_pass(size):
-    generate_pass = ''.join([random.SystemRandom().choice( string.ascii_uppercase +
-                                            string.digits)  
-                                            for n in range(size)])  
-                             
-    return generate_pass
